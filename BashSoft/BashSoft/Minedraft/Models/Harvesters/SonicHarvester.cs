0633fca2abfebe5b923b30dac52ee95588fdef4b
sammit neupane <sammit_neupane@hotmail.com>
2017-06-02 12:00:00 +0200

    fix: restructure Minedraft project cleanly

---
+++ BashSoft/BashSoft/Minedraft/Models/Harvesters/SonicHarvester.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+public class SonicHarvester : Harvester
+{
+    private int sonicFactor;
+
+    public int SonicFactor
+    {
+        get { return this.sonicFactor; }
+        private set { this.sonicFactor = value; }
+    }
+
+    public SonicHarvester(string id, double oreOutput, double energyRequirement, int sonicFactor) : base(id, oreOutput, energyRequirement)
+    {
+        this.SonicFactor = sonicFactor;
+        this.EnergyRequirement = (energyRequirement / sonicFactor);
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"Sonic Harvester - {this.Id}");
+        sb.AppendLine($"Ore Output: {this.OreOutput}");
+        sb.Append($"Energy Requirement: {this.EnergyRequirement}");
+
+        return sb.ToString();
+    }
+}
---
+++ BashSoft/BashSoft/Minedraft/Models/Machine.cs
@@ -0,0 +1,25 @@
+﻿
+
+// <summary>
+// Base class for all machines in the system, providing common properties and functionality.    
+// The Machine class is an abstract class that serves as a base for all types of machines in the Minedraft system,
+// including harvesters and providers. It contains a single property, Id, which is a string that uniquely identifies each machine.
+// The constructor of the Machine class takes an id parameter and sets the Id property. The Id property has a getter and a setter, allowing derived classes to access and modify the machine's identifier as needed. The Machine class provides a common foundation for all machines in the system, 
+// ensuring that they all have a unique identifier and can be managed consistently within the DraftManager.  
+// </ summary >
+
+public abstract class Machine
+{
+    private string id;
+
+    protected Machine(string id)
+    {
+        this.Id = id;
+    }
+
+    public string Id
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+}
